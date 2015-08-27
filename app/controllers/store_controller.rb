e434f45d0ad70a12b3f3218ce9e159bb5235ece0
vperez10 <vperez@chicagobooth.edu>
2015-08-27 08:29:00 -0400

    Working on CRUD features

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,8 +4,20 @@ def index
   @store = Store.all
   end
 
+def add
+     @store = Store.new
+    @store.first_name = params[:first_name]
+    @store.last_name = params[:last_name]
+    @store.email = params[:email]
+    @store.password = params[:password]
+
+    @store.save
+
+    render("index")
+end
+
 def update
-  render "update"
+  @store = Store.find(params[:id])
   end
 
   def view
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ get("/grocery/list", { :controller => "grocery", :action => "list" })
 get("/grocery/add", { :controller => "grocery", :action => "add" })
 get("/grocery/edit", { :controller => "grocery", :action => "edit" })
 
-get("/stores", { :controller => "store", :action => "index" })
-get("/stores/update", { :controller => "store", :action => "update" })
+get("/stores/add", { :controller => "store", :action => "add" })
+get("/stores/update/:id", { :controller => "store", :action => "update" })
 get("/stores/view", { :controller => "store", :action => "view" })
+get("/stores", { :controller => "store", :action => "index" })
 
 get("/login", { :controller => "customer", :action => "login" })
 get("/home", { :controller => "customer", :action => "home" })
